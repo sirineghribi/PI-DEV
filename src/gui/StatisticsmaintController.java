4c75338e0510e217c7af27a0fba9278bc9b5770f
AbderrahimAllani <abderrahim.allani@esprit.tn>
2023-03-09 01:28:45 +0100

    Added satistics and fixed a bug with the weather API

--- src/gui/StatisticsmaintController.java
+++ src/gui/StatisticsmaintController.java
@@ -7,7 +7,15 @@ package gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BubbleChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.StackedBarChart;
+import javafx.scene.chart.XYChart;
+import services.MaintenanceServices;
 
 /**
  * FXML Controller class
@@ -16,12 +24,28 @@ import javafx.fxml.Initializable;
  */
 public class StatisticsmaintController implements Initializable {
 
+    @FXML
+    private PieChart bar;
+    float total=0 ;
+    int i=0;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    ObservableList<PieChart.Data> datastat= FXCollections.observableArrayList();
+        new MaintenanceServices ().getAll().forEach((m)->total+=m.getCout());
+        new MaintenanceServices ().getAll().forEach((m)->{
+        float val=m.getCout();
+        String s="Maintenance:"+m.getId_m()+" "+(int)(val*100/total);
+        datastat.add(new PieChart.Data(s,val));
+        
+        });
+    bar.setData(datastat);
+        
+        
+        
     }    
     
 }
